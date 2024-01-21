0d16f5d428c6b613506c4d0212bbcf09cc4aef16
Glenn Hartmann <hartmann.glenn@gmail.com>
2024-01-21 14:12:07 -0500

    upgrade from go 1.19.5 to go 1.21.4

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/55070e598e0e03d1d116c49b9eff322ef07c6ac6; # go1.19.5 TODO
+    nixpkgs.url = github:NixOS/nixpkgs;
     flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
     flake-utils.url = "github:numtide/flake-utils";
   };
@@ -10,7 +10,7 @@
         pkgs = import nixpkgs { inherit system; };
         aoclib = pkgs.buildGoModule {
           pname = "aoclib";
-          version = "v0.0.4";
+          version = "v0.0.5";
           src = builtins.path { path = ./.; name = "aoclib"; };
           vendorHash = "sha256-WVWxbWhEpON9Gy9FuZbFkosK6lDJZk2NbZ0RaNVeLoU=";
         };
--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/glennhartmann/aoclib
 
-go 1.19
+go 1.21.4
 
 require golang.org/x/exp v0.0.0-20240119083558-1b970713d09a
